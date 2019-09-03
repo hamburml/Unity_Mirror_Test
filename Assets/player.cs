a623f0d29aa84f285f10ac69ad859607c9a28f82
hamburml <real.hamburml@gmail.com>
2019-09-03 20:28:01 +0200

    first commit
    first commit

---
+++ Assets/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class player : NetworkBehaviour
+{
+
+    private cube cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = FindObjectOfType<cube>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            cube.CmdRotate();
+        }
+
+    }
+}
